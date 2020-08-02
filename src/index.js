98090dcb8e923e3db7ae66b119b023f780a68ff4
Rodrigo Bondoc <bondoc.rodrigo@gmail.com>
2020-08-01 19:04:40 -0700

    added animeJS and animation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import anime from "animejs/lib/anime.es.js"
+
 import "./app/scss/index.scss"
 
-const name = document.querySelector("#company-name")
+anime({
+    targets: "#company-name",
+    translateY: 150,
+    duration: 1000,
+})
+
+anime({
+    targets: ".site-main__section",
+    translateY: 150,
+    duration: 1000,
+})
