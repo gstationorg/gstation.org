74b40f4e032e67e132288d36acdaffda7fb2a55a
Rodrigo Bondoc <bondoc.rodrigo@gmail.com>
2020-08-01 19:36:59 -0700

    removed animeJS animations

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import anime from "animejs/lib/anime.es.js"
-
 import "./app/scss/index.scss"
-
-anime({
-    targets: "#company-name",
-    translateY: 150,
-    duration: 1000,
-})
-
-anime({
-    targets: ".site-main__section",
-    translateY: 150,
-    duration: 1000,
-})
